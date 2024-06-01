c9bc6c71f1935c075718b8ca4e699ca3b5823717
SergioAndrade22 <sergio.david2201@gmail.com>
2024-06-01 02:02:26 -0300

    Adds en passant capture

--- src/app/chess-logic/chess-board.ts
+++ src/app/chess-logic/chess-board.ts
@@ -227,6 +227,8 @@ export class ChessBoard {
                         pieceSafeSquares.push({x, y: 6})
                     if (this.canCastle(piece, false)) // queen side
                         pieceSafeSquares.push({x, y: 2})
+                } else if (piece instanceof Pawn && this.canCaptureEnPassant(piece, x, y)) {
+                    pieceSafeSquares.push({ x: x + (piece.color === Color.White ? 1 : -1), y: this._lastMove!.prevY })
                 }
 
                 if (pieceSafeSquares.length) {
@@ -236,6 +238,28 @@ export class ChessBoard {
         }
 
         return safeSquares
+    }
+
+    private canCaptureEnPassant(pawn: Pawn, pawnX: number, pawnY: number): boolean {
+        if (!this._lastMove) return false
+        const { piece, prevX, currX, currY } = this._lastMove
+
+        if (
+            !(piece instanceof Pawn) || // Only pawns can capture en passant
+            pawn.color !== this._playerTurn || // The last pawn moved doesn't belong to this player
+            Math.abs(currX - prevX) !== 2 || // En passant condition 1: the enemy moved his pawn 2 squares
+            pawnX !== currX || // En passant condition 2: both the enemy pawn moved on his last turn and the current player one are on the same row
+            Math.abs(pawnY - currY) !== 1 // En passant condition 3: both pawns are adjacent to each other
+        ) return false
+        
+        const pawnNewPositionX: number = pawnX + (pawn.color === Color.White ? 1 : -1)
+        const pawnNewPositionY: number = currY
+
+        this.chessBoard[currX][currY] = null
+        const isPositionSafe: boolean = this.isPositionSafeAfterMove(pawn, pawnX, pawnY, pawnNewPositionX, pawnNewPositionY)
+        this.chessBoard[currX][currY] = piece
+
+        return isPositionSafe
     }
 
     public canCastle(king: King, kingSideCastle: boolean): boolean {
@@ -311,6 +335,15 @@ export class ChessBoard {
             this.chessBoard[rookPositionX][rookPositionY] = null
             this.chessBoard[rookPositionX][rookNewPositionY] = rook
             rook.hasMoved = true
+        } else if ( // En passant happened
+            piece instanceof Pawn &&
+            this._lastMove !== undefined &&
+            this._lastMove.piece instanceof Pawn &&
+            Math.abs(this._lastMove.currX - this._lastMove.prevX) === 2 &&
+            prevX === this._lastMove.currX &&
+            newY === this._lastMove.currY
+        ) {
+            this.chessBoard[this._lastMove.currX][this._lastMove.currY] = null
         }
     }
 }
